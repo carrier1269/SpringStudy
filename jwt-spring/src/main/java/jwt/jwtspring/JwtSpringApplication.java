b099860c72d0307ee03a33db11fed8c1e3d5bfee
carrier1269 <carrier1269@naver.com>
2023-08-23 17:50:02 +0900

    jwt-spring

---
+++ jwt-spring/src/main/java/jwt/jwtspring/JwtSpringApplication.java
@@ -0,0 +1,13 @@
+package jwt.jwtspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringApplication.class, args);
+	}
+
+}
---
+++ jwt-spring/src/test/java/jwt/jwtspring/JwtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jwt.jwtspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
